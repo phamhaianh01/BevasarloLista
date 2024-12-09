1611ce4b70b5d9440872113c43fff35e12d73b98
Pham Hai Anh <75503824+phamhaianh01@users.noreply.github.com>
2024-12-09 02:11:18 +0100

    Backend

---
+++ Backend/BevasarloLista/BevasarloLista.Api/Controllers/CalculateController.cs
@@ -0,0 +1,50 @@
+﻿using BevasarloLista.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BevasarloLista.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        private ListDbContext dbContext;
+
+        public CalculateController(ListDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Double>> Get(int year, int month, int userId)
+        {
+            var date = new DateTime(year, month, 1);
+            var user = dbContext.Users.Find(userId);
+
+            var result = Calculate(user, dbContext.Users.Count(), dbContext.Items.AsQueryable(), date);
+            return Ok(result);
+            
+        }
+
+        public static double Calculate(User currentUser, int userCount, IQueryable<Item> items, DateTime date)
+        {
+            double sum = 0;
+
+            sum += items.Where(i => i.For == currentUser && i.CheckedBy != currentUser
+                && i.PurchaseDate.Month == date.Month && i.PurchaseDate.Year == date.Year)
+                .Select(i => i.Price * i.Amount).Sum(); //AMI AZ ÖVÉ, DE NEM Ő VETT MEG
+
+            sum += items.Where(i => i.For == null
+             && i.PurchaseDate.Month == date.Month && i.PurchaseDate.Year == date.Year)
+                .Select(i => i.Price * i.Amount / userCount)
+                .Sum(); //KÖZÖS
+
+            sum -= items.Where(i => i.For == null && i.CheckedBy == currentUser
+             && i.PurchaseDate.Month == date.Month && i.PurchaseDate.Year == date.Year)
+                .Select(i => i.Price * i.Amount / userCount).Sum(); // KÖZÖS, DE Ö VETTE MEG
+
+            return sum;
+        }
+
+    }
+}
---
+++ Backend/BevasarloLista/BevasarloLista.Api/Controllers/ItemController.cs
@@ -0,0 +1,74 @@
+﻿using BevasarloLista.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BevasarloLista.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private ListDbContext dbContext;
+
+        public ItemController(ListDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> Get()
+        {
+            var itemList = await dbContext.Items.ToListAsync();
+            return Ok(itemList);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> GetId(int id)
+        {
+            
+            var item = await dbContext.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+             
+            return Ok(item);
+           
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put([FromBody] Item item)
+        {
+            dbContext.Items.Update(item);
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Item item)
+        {
+
+            dbContext.Items.Add(item);
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var item = dbContext.Items.Find(id);
+            if (item != null)
+            {
+                dbContext.Items.Remove(item);
+                return Ok();
+            }
+
+            return NotFound();
+            
+        }
+
+
+
+    }
+}
